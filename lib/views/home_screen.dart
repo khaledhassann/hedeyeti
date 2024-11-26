6f903e5ad4dae0947da45b346356c6f134813bbe
khaledhassann <kmhtaha@gmail.com>
2024-11-26 14:28:10 +0200

    Now home page uses models

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,112 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:hedeyeti/models/Event.dart';
+import 'package:hedeyeti/models/Gift.dart';
+import 'package:hedeyeti/models/User.dart';
 
 class HomePage extends StatelessWidget {
-  final List<Map<String, dynamic>> friends = [
-    {
-      'name': 'Khaled Taha',
-      'profilePicture': 'assets/images.png',
-      'upcomingEvents': 2,
-      'events': [
-        {
-          'name': 'Birthday Party',
-          'date': '2024-12-10',
-          'category': 'Birthday',
-          'gifts': [
-            {
-              'name': 'Smartphone',
-              'category': 'Electronics',
-              'price': 799.99,
-              'status': 'Available'
-            },
-            {
-              'name': 'Book: Flutter for Beginners',
-              'category': 'Books',
-              'price': 19.99,
-              'status': 'Available'
-            },
-          ],
-        },
-        {
-          'name': 'Wedding',
-          'date': '2024-11-30',
-          'category': 'Wedding',
-          'gifts': [
-            {
-              'name': 'Blender',
-              'category': 'Home Appliances',
-              'price': 149.99,
-              'status': 'Available'
-            },
-          ],
-        },
-      ],
-    },
-    {
-      'name': 'John Doe',
-      'profilePicture': 'assets/man02.png',
-      'upcomingEvents': 2,
-      'events': [
-        {
-          'name': 'Birthday Party',
-          'date': '2024-12-10',
-          'category': 'Birthday',
-          'gifts': [
-            {
-              'name': 'Smartphone',
-              'category': 'Electronics',
-              'price': 799.99,
-              'status': 'Available'
-            },
-            {
-              'name': 'Book: Flutter for Beginners',
-              'category': 'Books',
-              'price': 19.99,
-              'status': 'Available'
-            },
-          ],
-        },
-        {
-          'name': 'Wedding',
-          'date': '2024-11-30',
-          'category': 'Wedding',
-          'gifts': [
-            {
-              'name': 'Blender',
-              'category': 'Home Appliances',
-              'price': 149.99,
-              'status': 'Available'
-            },
-          ],
-        },
-      ],
-    },
-    {
-      'name': 'Jane Smith',
-      'profilePicture': 'assets/girl01.png',
-      'upcomingEvents': 0,
-      'events': [],
-    },
-    {
-      'name': 'Emily Johnson',
-      'profilePicture': 'assets/man01.png',
-      'upcomingEvents': 1,
-      'events': [
-        {
-          'name': 'Graduation Party',
-          'date': '2024-09-15',
-          'category': 'Graduation',
-          'gifts': [
-            {
-              'name': 'Laptop',
-              'category': 'Electronics',
-              'price': 999.99,
-              'status': 'Available'
-            },
-          ],
-        },
-      ],
-    },
-  ];
+  late final List<Gift> gifts;
+  late final List<Event> events;
+  late final List<User> exampleUsers;
+
+  HomePage({Key? key}) : super(key: key) {
+    // Initialize gifts
+    gifts = [
+      Gift(
+          name: "Smartphone",
+          category: 'Electronics',
+          price: 799.99,
+          status: 'Available'),
+      Gift(
+          name: 'Blender',
+          category: 'Home Appliances',
+          price: 149.99,
+          status: 'Available'),
+      Gift(
+          name: 'Laptop',
+          category: 'Electronics',
+          price: 1499.75,
+          status: 'Pledged'),
+      Gift(
+          name: 'Flutter for Beginners',
+          category: 'Books',
+          price: 24.99,
+          status: 'Available'),
+    ];
+
+    // Initialize events
+    events = [
+      Event(
+          name: 'Birthday party',
+          date: DateTime(2024, 12, 15),
+          category: 'Birthday',
+          gifts: gifts),
+      Event(
+          name: 'Wedding',
+          date: DateTime(2025, 9, 30),
+          category: 'Wedding',
+          gifts: gifts),
+      Event(
+          name: 'Graduation party',
+          date: DateTime(2025, 7, 1),
+          category: 'Graduation',
+          gifts: gifts),
+    ];
+
+    // Initialize users
+    exampleUsers = [
+      User(
+          name: 'Khaled Taha',
+          email: 'khaled@email.com',
+          profilePicture: 'assets/images.png',
+          isMe: true,
+          events: events),
+      User(
+          name: 'John Doe',
+          email: 'john.doe@email.com',
+          profilePicture: 'assets/man02.png',
+          isMe: false,
+          events: events),
+      User(
+          name: 'Jane Smith',
+          email: 'jane.smith@email.com',
+          profilePicture: 'assets/girl01.png',
+          isMe: false,
+          events: []),
+      User(
+          name: 'Pablo Escobar',
+          email: 'pablo.escobar@email.com',
+          profilePicture: 'assets/man01.png',
+          isMe: false,
+          events: events),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -117,14 +90,13 @@ class HomePage extends StatelessWidget {
           builder: (context) {
             return GestureDetector(
               onTap: () {
-                Scaffold.of(context).openDrawer(); // Open drawer on avatar tap
+                Scaffold.of(context).openDrawer();
               },
               child: const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 11.0),
                 child: CircleAvatar(
                   radius: 18,
-                  backgroundImage:
-                      AssetImage('assets/images.png'), // Placeholder
+                  backgroundImage: AssetImage('assets/images.png'),
                 ),
               ),
             );
@@ -139,124 +111,131 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      drawer: Drawer(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                ),
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      child: CircleAvatar(
-                        radius: 40,
-                        backgroundImage: AssetImage('assets/images.png'),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    FittedBox(
-                      child: Text(
-                        'Khaled Taha',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        'khaled@email.com',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.event),
-                title: const Text('My Events'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/events', arguments: {
-                    'name': 'Khaled Taha', // Placeholder for user name
-                    'events': friends.firstWhere(
-                        (friend) => friend['name'] == 'Khaled Taha')['events'],
-                  });
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.card_giftcard),
-                title: const Text('My Pledged Gifts'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/pledged-gifts');
-                },
-              ),
-              const Divider(),
-              ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text('Manage profile'),
-                onTap: () {
-                  // TODO: Navigate to settings page
-                  Navigator.pushNamed(context, '/profile');
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.logout),
-                title: const Text('Logout'),
-                onTap: () {
-                  // TODO: Implement logout functionality
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+      drawer: _buildDrawer(context),
       body: Column(
         children: [
-          // Button to create a new event or list
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/create-edit-event');
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-              ),
-              child: const Text('Create Your Own Event/List'),
-            ),
-          ),
-          // Friends List
-          Expanded(
-            child: ListView.builder(
-              itemCount: friends.length,
-              itemBuilder: (context, index) {
-                if (index == 0) return Center();
-                final friend = friends[index];
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: AssetImage(friend['profilePicture']),
-                  ),
-                  title: Text(friend['name']),
-                  subtitle: Text(
-                    friend['upcomingEvents'] > 0
-                        ? 'Upcoming Events: ${friend['upcomingEvents']}'
-                        : 'No Upcoming Events',
-                  ),
-                  trailing: const Icon(Icons.chevron_right),
-                  onTap: () {
-                    // Navigate to Event List Page with friend's data
-                    Navigator.pushNamed(
-                      context,
-                      '/events',
-                      arguments: friend, // Pass the friend's data
-                    );
-                  },
-                );
-              },
-            ),
-          ),
+          _buildCreateEventButton(context),
+          _buildFriendsList(),
         ],
+      ),
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage('assets/images.png'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  FittedBox(
+                    child: Text(
+                      'Khaled Taha',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                  FittedBox(
+                    child: Text(
+                      'khaled@email.com',
+                      style: TextStyle(color: Colors.white70),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.event),
+              title: const Text('My Events'),
+              onTap: () {
+                Navigator.pushNamed(context, '/events', arguments: {
+                  'name': 'Khaled Taha',
+                  'events': exampleUsers.first.events,
+                });
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.card_giftcard),
+              title: const Text('My Pledged Gifts'),
+              onTap: () {
+                Navigator.pushNamed(context, '/pledged-gifts');
+              },
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Manage profile'),
+              onTap: () {
+                Navigator.pushNamed(context, '/profile');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {
+                // TODO: Implement logout functionality
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCreateEventButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/create-edit-event');
+        },
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(double.infinity, 50),
+        ),
+        child: const Text('Create Your Own Event/List'),
+      ),
+    );
+  }
+
+  Widget _buildFriendsList() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: exampleUsers.length,
+        itemBuilder: (context, index) {
+          final user = exampleUsers[index];
+          if (user.isMe) return const SizedBox();
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(user.profilePicture),
+            ),
+            title: Text(user.name),
+            subtitle: Text(
+              user.events.isNotEmpty
+                  ? 'Upcoming Events: ${user.events.length}'
+                  : 'No Upcoming Events',
+            ),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/events',
+                arguments: {'name': user.name, 'events': user.events},
+              );
+            },
+          );
+        },
       ),
     );
   }
